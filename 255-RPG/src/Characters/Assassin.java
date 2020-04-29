efc3e42a3860e2368ab420202c5828795715039f
SeanButters <32407246+SeanButters@users.noreply.github.com>
2020-04-29 18:22:18 -0400

    Added mage and ranger 'class' to match setup screen

--- 255-RPG/src/Characters/Assassin.java
+++ 255-RPG/src/Characters/Assassin.java
@@ -1,1 +1,17 @@
 
+package Characters;
+
+/**
+ *
+ * @author sean
+ */
+public class Assassin extends PlayableCharacter{
+    public Assassin(){
+        setMaxhp(12);
+        setCurrenthp(12);
+        setLevel(1);
+        setClassname("Assassin");
+        setXp(0);
+        setAttackdmg(9);
+    }
+}
--- 255-RPG/src/Characters/Mage.java
+++ 255-RPG/src/Characters/Mage.java
@@ -1,1 +1,16 @@
+package Characters;
 
+/**
+ *
+ * @author sean
+ */
+public class Mage extends PlayableCharacter{
+    public Mage(){
+        setMaxhp(14);
+        setCurrenthp(14);
+        setLevel(1);
+        setClassname("Mage");
+        setXp(0);
+        setAttackdmg(7);
+    }
+}
